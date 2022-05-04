add68a86f790fca6d5e4448c74a18a1b8a277ea4
MMIINNNN <100738705+MMIINNNN@users.noreply.github.com>
2022-05-04 09:52:28 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_packge_data = True,
+  name= 'mycalc4656',
+  version='0.0.1',
+  description='oss-dev my calculator'
+  autuor='minseokkim'
+  autuor_email='1810091@donga.ac.kr'
+url= "http://github.com/MMINNNN/calc0001"
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdawn',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
